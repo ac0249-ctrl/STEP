44e4889795543fc38e9acbd9eea8f51988132532
ac0249-ctrl <ac0249@srmist.edu.in>
2025-08-16 21:43:10 +0530

    String Week1

---
+++ CustomSplit.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class CustomSplit {
+    public static int findLength(String str) {
+        int count = 0;
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+        }
+        return count;
+    }
+    public static String[] customSplit(String text) {
+        int len = findLength(text);
+        int wordCount = 1;
+        for (int i = 0; i < len; i++) {
+            if (text.charAt(i) == ' ') {
+                wordCount++;
+            }
+        }
+        int[] spaceIndexes = new int[wordCount + 1];
+        int index = 0;
+        spaceIndexes[index++] = -1;
+
+        for (int i = 0; i < len; i++) {
+            if (text.charAt(i) == ' ') {
+                spaceIndexes[index++] = i;
+            }
+        }
+        spaceIndexes[index] = len;
+        String[] words = new String[wordCount];
+        for (int i = 0; i < wordCount; i++) {
+            int start = spaceIndexes[i] + 1;
+            int end = spaceIndexes[i + 1];
+            String word = "";
+            for (int j = start; j < end; j++) {
+                word += text.charAt(j);
+            }
+            words[i] = word;
+        }
+
+        return words;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = sc.nextLine();
+        String[] customWords = customSplit(input);
+        String[] builtInWords = input.split(" ");
+        System.out.println("\nCustom Split Result");
+        for (String word : customWords) {
+            System.out.println(word);
+        }
+
+        System.out.println("\nBuilt-in Split Result");
+        for (String word : builtInWords) {
+            System.out.println(word);
+        }
+
+        sc.close();
+    }
+}
